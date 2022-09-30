87feac8524864a813ef41678263622ff2fa71de6
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-10-01 02:30:49 +0300

    improve algorithm

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict):
-        return max(a_dictionary)
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    
+    res = list(a_dictionary.keys())[0]
+    big = a_dictionary[res]
+    for key, value in a_dictionary.items():
+        if value > big:
+            big = value
+            res = key
+    return res
