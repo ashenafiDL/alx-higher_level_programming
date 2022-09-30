97d2a56fc513c8d7fc30dd0b4327a2c9a232474d
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-10-01 02:32:15 +0300

    remove trailing whitespace

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    
+
     res = list(a_dictionary.keys())[0]
     big = a_dictionary[res]
     for key, value in a_dictionary.items():
