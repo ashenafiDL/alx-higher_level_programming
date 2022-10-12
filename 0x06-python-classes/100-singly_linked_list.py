0b75c322d5efc08e595b07d2ff69b61b2e7d4bc6
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-10-12 18:08:49 +0300

    added advanced tasks 7 and 8

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,7 +3,7 @@
 
 
 class Node:
-    """A singly node of a singly linked list"""
+    """Defines a new node for singly linked list"""
 
     def __init__(self, data, next_node=None):
         """Initializes a new node
@@ -29,20 +29,61 @@ class Node:
     @property
     def next_node(self):
         """The next node in the list"""
-        return self.__netx_node
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) or value != None:
+        if value != None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
-class SinlyLinkedList:
+class SinglyLinkedList:
     """Defines a singly linked list data structure"""
 
     def __init__(self):
-        """"""
+        """Initializes a new singly linked list"""
+        self.__head = None
 
     def sorted_insert(self, value):
-        """"""
+        """Inserts a new node into the singly linked list
+
+        The new node will be inserted at the
+        correct position of increasingly sorted items
+
+        Args:
+            value (int): the new data to be inserted
+        """
+        new_node = Node(value)
+
+        temp = self.__head
+        if self.__head == None:
+            self.__head = new_node
+        elif temp.data >= value:
+            self.__head = new_node
+            new_node.next_node = temp
+        else:
+            while temp.next_node != None:
+                if temp.next_node.data >= value:
+                    break
+                temp = temp.next_node
+
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
+
+    def __str__(self) -> str:
+        """Makes a SinglyLinkedList object printable
+
+        Prints the entire list in stdout, one node number by line
+
+        Returns:
+            the list items as strings
+        """
+        datas = []
+
+        temp = self.__head
+        while temp != None:
+            datas.append(str(temp.data))
+            temp = temp.next_node
+
+        return ('\n'.join(datas))
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""This module defines a class called Square.
+"""
+
+
+class Square:
+    """Represents a square with a size
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes a new square object with a size
+
+        Args:
+            size (int): the size of the sqaure.
+            The size must be positive number (obviously)
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """int: the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        """tuple: the coordinates of the square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple)
+                or len(value) != 2
+                or not all(isinstance(xy, int) for xy in value)
+                or not all(xy >= 0 for xy in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """Computes the area of the square object
+
+        Returns:
+            The are of the current sqaure object
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Prints the square with the character #
+        """
+
+        if self.__size == 0:
+            print("")
+        else:
+            # first print empty lines for the amount of the y coordinate
+            for y in range(0, self.__position[1]):
+                print("")
+
+            for height in range(0, self.__size):
+                # before printing the row first print " "
+                # for the amount of x coordinate
+                for x in range(0, self.__position[0]):
+                    print(' ', end='')
+                for width in range(0, self.__size):
+                    print("#", end='')
+                print ("")
+
+    def __str__(self) -> str:
+        """Makes Square object printable"""
+
+        if self.__size != 0:
+            for y in range(0, self.__position[1]):
+                print("")
+        for height in range(0, self.__size):
+            for x in range(0, self.__position[0]):
+                print(' ', end='')
+            for width in range(0, self.__size):
+                print("#", end='')
+            if height != self.__size - 1:
+                print("")
+        return ("")
