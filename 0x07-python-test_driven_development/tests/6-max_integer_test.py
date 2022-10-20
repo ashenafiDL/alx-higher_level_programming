9e4cd792e48e453eb7a70bac2d7d1d17d277ba7d
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-10-20 17:58:25 +0300

    fix typo

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,7 +9,7 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_int(self):
         """Tests for integers"""
-        self.assertEqual(max_integer([98]), 4)
+        self.assertEqual(max_integer([98]), 98)
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertEqual(max_integer([85, 6, 3, 0, 2]), 85)
@@ -18,7 +18,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_float(self):
         """Tests for floats"""
         self.assertEqual(max_integer([1.620]), 1.620)
-        self.assertEqual(max_integer([5.3, -9.5, 6, 0, 10.62]), 10.61)
+        self.assertEqual(max_integer([5.3, -9.5, 6, 0, 10.62]), 10.62)
 
     def test_string(self):
         """Tests for strings"""
