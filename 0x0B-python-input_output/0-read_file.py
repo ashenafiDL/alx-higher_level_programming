93577f66a64ef354bbe481ff2930d42436ab5e96
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-10-25 16:44:27 +0300

    correct output formating

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename="") -> None:
         Defaults to "".
     """
     with open(filename, mode='r', encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
