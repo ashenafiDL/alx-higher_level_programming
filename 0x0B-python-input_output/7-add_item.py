6727eb6d2cd66fbf4e2d171e944e00bef2d4d7fe
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-10-26 08:47:23 +0300

    fixx

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a script that adds all arguments to a Python list,
+"""a script that adds all arguments to a python list,
 and then save them to a file
 """
 from sys import argv
